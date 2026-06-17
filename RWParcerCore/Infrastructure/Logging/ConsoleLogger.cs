c663853b63cd09bb4e1fb8c044e9ef360aace6e5
danilaltd <danilaltd2006@gmail.com>
2026-06-17 14:21:15 +0300

    hide timestaps in docker as docker stores it

--- RWParcerCore/Infrastructure/Logging/ConsoleLogger.cs
+++ RWParcerCore/Infrastructure/Logging/ConsoleLogger.cs
@@ -4,30 +4,36 @@ namespace RWParcerCore.Infrastructure.Logging
 {
     public class ConsoleLogger : ILogger
     {
-        private string Now() => DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.fff");
+        private static readonly bool IsInDocker = 
+            Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true";
+
+        private string Now() => IsInDocker 
+            ? "" 
+            : DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.fff ") ;
+
         public void LogError(Exception ex, string message, params object[] args)
         {
-            Console.WriteLine($"{Now()} [ERROR] {string.Format(message, args)}");
+            Console.WriteLine($"{Now()}[ERROR] {string.Format(message, args)}");
             if (ex != null)
             {
-                Console.WriteLine($"{Now()} Exception: {ex.Message}");
-                Console.WriteLine($"{Now()} Stack Trace: {ex.StackTrace}");
+                Console.WriteLine($"{Now()}Exception: {ex.Message}");
+                Console.WriteLine($"{Now()}Stack Trace: {ex.StackTrace}");
             }
         }
 
         public void LogWarning(string message, params object[] args)
         {
-            Console.WriteLine($"{Now()} [WARNING] {string.Format(message, args)}");
+            Console.WriteLine($"{Now()}[WARNING] {string.Format(message, args)}");
         }
 
         public void LogInfo(string message, params object[] args)
         {
-            Console.WriteLine($"{Now()} [INFO] {string.Format(message, args)}");
+            Console.WriteLine($"{Now()}[INFO] {string.Format(message, args)}");
         }
 
         public void LogDebug(string message, params object[] args)
         {
-            Console.WriteLine($"{Now()} [DEBUG] {string.Format(message, args)}");
+            Console.WriteLine($"{Now()}[DEBUG] {string.Format(message, args)}");
         }
     }
 }
